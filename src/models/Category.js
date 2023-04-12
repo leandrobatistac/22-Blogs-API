560781d8e27ece26b17899d02084a845515e6c7e
Leandro <leandrobcrv@gmail.com>
2023-04-12 06:45:56 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: DataTypes.STRING
+  }, {
+    tableName: 'categories',
+    underscored: true,
+    timestamps: false,
+  })
+
+  return CategoryTable;
+}
